3fc4ff12e07317da6b925c9d2a967d6686f7fb38
lidia783 <159478887+lidia783@users.noreply.github.com>
2024-06-11 12:06:33 +0300

    Create AniDeStudii.Designer.cs

---
+++ AniDeStudii.Designer.cs
@@ -0,0 +1,402 @@
+
+namespace Programe
+{
+    partial class ANiDeStudii
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ANiDeStudii));
+            panel1 = new Panel();
+            button13 = new Button();
+            imageList1 = new ImageList(components);
+            button12 = new Button();
+            button11 = new Button();
+            button10 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            button6 = new Button();
+            button9 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button13);
+            panel1.Controls.Add(button12);
+            panel1.Controls.Add(button11);
+            panel1.Controls.Add(button10);
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button9);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1078, 828);
+            panel1.TabIndex = 0;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.White;
+            button13.Dock = DockStyle.Top;
+            button13.FlatAppearance.BorderSize = 0;
+            button13.FlatStyle = FlatStyle.Flat;
+            button13.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button13.ForeColor = Color.FromArgb(106, 176, 160);
+            button13.ImageIndex = 3;
+            button13.ImageList = imageList1;
+            button13.Location = new Point(0, 765);
+            button13.Name = "button13";
+            button13.Size = new Size(1078, 60);
+            button13.TabIndex = 21;
+            button13.Text = "   Medicină";
+            button13.TextAlign = ContentAlignment.MiddleRight;
+            button13.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += button13_Click;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "drept.png");
+            imageList1.Images.SetKeyName(1, "iesc.png");
+            imageList1.Images.SetKeyName(2, "litere.png");
+            imageList1.Images.SetKeyName(3, "medicina.png");
+            imageList1.Images.SetKeyName(4, "muzica.png");
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(181, 216, 208);
+            button12.Dock = DockStyle.Top;
+            button12.FlatAppearance.BorderSize = 0;
+            button12.FlatStyle = FlatStyle.Flat;
+            button12.Font = new Font("Georgia", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button12.ForeColor = Color.White;
+            button12.Location = new Point(0, 690);
+            button12.Name = "button12";
+            button12.Size = new Size(1078, 75);
+            button12.TabIndex = 20;
+            button12.Text = "6 ani";
+            button12.TextAlign = ContentAlignment.MiddleLeft;
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.White;
+            button11.Dock = DockStyle.Top;
+            button11.FlatAppearance.BorderSize = 0;
+            button11.FlatStyle = FlatStyle.Flat;
+            button11.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button11.ForeColor = Color.FromArgb(106, 176, 160);
+            button11.ImageIndex = 3;
+            button11.ImageList = imageList1;
+            button11.Location = new Point(0, 630);
+            button11.Name = "button11";
+            button11.Size = new Size(1078, 60);
+            button11.TabIndex = 19;
+            button11.Text = "   Asistență medicală generală";
+            button11.TextAlign = ContentAlignment.MiddleRight;
+            button11.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += button11_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.White;
+            button10.Dock = DockStyle.Top;
+            button10.FlatAppearance.BorderSize = 0;
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button10.ForeColor = Color.FromArgb(106, 176, 160);
+            button10.ImageIndex = 4;
+            button10.ImageList = imageList1;
+            button10.Location = new Point(0, 570);
+            button10.Name = "button10";
+            button10.Size = new Size(1078, 60);
+            button10.TabIndex = 18;
+            button10.Text = "   Interpretare muzicală – instrumente (română, engleză)";
+            button10.TextAlign = ContentAlignment.MiddleRight;
+            button10.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.White;
+            button8.Dock = DockStyle.Top;
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button8.ForeColor = Color.FromArgb(106, 176, 160);
+            button8.ImageIndex = 4;
+            button8.ImageList = imageList1;
+            button8.Location = new Point(0, 510);
+            button8.Name = "button8";
+            button8.Size = new Size(1078, 60);
+            button8.TabIndex = 17;
+            button8.Text = "   Interpretare muzicală – canto";
+            button8.TextAlign = ContentAlignment.MiddleRight;
+            button8.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.White;
+            button7.Dock = DockStyle.Top;
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.ForeColor = Color.FromArgb(106, 176, 160);
+            button7.ImageIndex = 1;
+            button7.ImageList = imageList1;
+            button7.Location = new Point(0, 450);
+            button7.Name = "button7";
+            button7.Size = new Size(1078, 60);
+            button7.TabIndex = 16;
+            button7.Text = "   Facultatea de Inginerie electrică și știința calculatoarelor";
+            button7.TextAlign = ContentAlignment.MiddleRight;
+            button7.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.White;
+            button5.Dock = DockStyle.Top;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.ForeColor = Color.FromArgb(106, 176, 160);
+            button5.ImageIndex = 0;
+            button5.ImageList = imageList1;
+            button5.Location = new Point(0, 390);
+            button5.Name = "button5";
+            button5.Size = new Size(1078, 60);
+            button5.TabIndex = 15;
+            button5.Text = "   Facultatea de Drept";
+            button5.TextAlign = ContentAlignment.MiddleRight;
+            button5.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(181, 216, 208);
+            button4.Dock = DockStyle.Top;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Georgia", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(0, 315);
+            button4.Name = "button4";
+            button4.Size = new Size(1078, 75);
+            button4.TabIndex = 14;
+            button4.Text = "4 ani";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.White;
+            button3.Dock = DockStyle.Top;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.FromArgb(106, 176, 160);
+            button3.ImageIndex = 3;
+            button3.ImageList = imageList1;
+            button3.Location = new Point(0, 255);
+            button3.Name = "button3";
+            button3.Size = new Size(1078, 60);
+            button3.TabIndex = 13;
+            button3.Text = "   Laborator clinic";
+            button3.TextAlign = ContentAlignment.MiddleRight;
+            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.Dock = DockStyle.Top;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.FromArgb(106, 176, 160);
+            button2.ImageIndex = 3;
+            button2.ImageList = imageList1;
+            button2.Location = new Point(0, 195);
+            button2.Name = "button2";
+            button2.Size = new Size(1078, 60);
+            button2.TabIndex = 12;
+            button2.Text = "   Balneofiziokinetoterapie și recuperare";
+            button2.TextAlign = ContentAlignment.MiddleRight;
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.Dock = DockStyle.Top;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.FromArgb(106, 176, 160);
+            button1.ImageIndex = 4;
+            button1.ImageList = imageList1;
+            button1.Location = new Point(0, 135);
+            button1.Name = "button1";
+            button1.Size = new Size(1078, 60);
+            button1.TabIndex = 11;
+            button1.Text = "   Muzică";
+            button1.TextAlign = ContentAlignment.MiddleRight;
+            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.White;
+            button6.Dock = DockStyle.Top;
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Georgia", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.ForeColor = Color.FromArgb(106, 176, 160);
+            button6.ImageIndex = 2;
+            button6.ImageList = imageList1;
+            button6.Location = new Point(0, 75);
+            button6.Name = "button6";
+            button6.Size = new Size(1078, 60);
+            button6.TabIndex = 10;
+            button6.Text = "   Facultatea de Litere";
+            button6.TextAlign = ContentAlignment.MiddleRight;
+            button6.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(181, 216, 208);
+            button9.Dock = DockStyle.Top;
+            button9.FlatAppearance.BorderSize = 0;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Georgia", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.ForeColor = Color.White;
+            button9.Location = new Point(0, 0);
+            button9.Name = "button9";
+            button9.Size = new Size(1078, 75);
+            button9.TabIndex = 9;
+            button9.Text = "3 ani";
+            button9.TextAlign = ContentAlignment.MiddleLeft;
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            ClientSize = new Size(1100, 630);
+            Controls.Add(panel1);
+            Name = "Form3";
+            Text = "Form3";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button9;
+        private Button button6;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button button13;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private Button button8;
+        private Button button7;
+        private Button button5;
+        private Button button4;
+        private ImageList imageList1;
+    }
+}
